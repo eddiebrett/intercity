448f202f185259836571b04ed4524d3e7664e14f
eddiebrett <eddiebrett@hotmail.co.uk>
2020-05-28 14:57:40 +0000

    added navbar

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/candidates')
+def candidates():
+    return render_template('candidates.html')
+
+@app.route('/clients')
+def clients():
+    return render_template('clients.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
