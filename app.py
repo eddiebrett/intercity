ce9190e9dc27f55160e6516ce7189133851a2de8
eddiebrett <eddiebrett@hotmail.co.uk>
2020-07-21 20:24:47 +0000

    initial reboot

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, flash
-from forms import ContactForm
+from flask import Flask, render_template
+
+# , request
+# from forms import ContactForm
 
 app = Flask(__name__)
 
-app.secret_key = 'development key'
+# app.secret_key = 'development key'
 
 
 @app.route('/')
@@ -26,16 +28,16 @@ def legal():
     return render_template('legal.html')
 
 
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-    form = ContactForm()
+# @app.route('/contact', methods=['GET', 'POST'])
+# def contact():
+#     form = ContactForm()
 
-    if request.method == 'POST':
-        if form.validate() is False:
-            flash('All fields are required.')
-            return render_template('contact.html', form=form)
-        else:
-            return 'Form posted.'
+#     if request.method == 'POST':
+#         if form.validate() is False:
+#             flash('All fields are required.')
+#             return render_template('contact.html', form=form)
+#         else:
+#             return 'Form posted.'
 
-    elif request.method == 'GET':
-        return render_template('contact.html', form=form)
+#     elif request.method == 'GET':
+#         return render_template('contact.html', form=form)
