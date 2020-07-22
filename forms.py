d593ca5758bf2e5d9bb65db78277ca4c3b42f98d
eddiebrett <eddiebrett@hotmail.co.uk>
2020-07-22 17:19:52 +0000

    update validators

--- forms.py
+++ forms.py
@@ -1,13 +1,23 @@
-from flask_wtf import Form
-from wtforms.fields import TextField, BooleanField, SubmitField, TextAreaField
+# from flask_wtf import Form
+# from wtforms.fields import TextField, BooleanField, SubmitField, TextAreaField
 
-from wtforms import validators
-from wtforms.validators import Required, ValidationError
+# from wtforms import validators
+# from wtforms.validators import Required, ValidationError
 
 
+# class ContactForm(Form):
+#     name = TextField("Name",  [validators.Required("Please enter your name.")])
+#     email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+#     subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
+#     message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
+#     submit = SubmitField("Send")
+
+
+from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
+ 
 class ContactForm(Form):
-    name = TextField("Name",  [validators.Required()])
-    email = TextField("Email",  [validators.Required(), validators.Email()])
-    subject = TextField("Subject",  [validators.Required()])
-    message = TextAreaField("Message",  [validators.Required()])
-    submit = SubmitField("Send")
+  name = TextField("Name",  [validators.Required("Please enter your name.")])
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
+  message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
+  submit = SubmitField("Send")
