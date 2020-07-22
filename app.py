7a75fe965f16889b683f3a63dbec8fc94ea165d6
eddiebrett <eddiebrett@hotmail.co.uk>
2020-07-22 18:24:11 +0000

    adding email

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template, request, flash
 from forms import ContactForm
+from flask import Message, Mail
+
+
+mail = Mail()
 
 app = Flask(__name__)
 
 app.secret_key = 'development key'
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'bobdylan20202020@gmail.com'
+app.config["MAIL_PASSWORD"] = 'your-password'
+ 
+mail.init_app(app)
 
 
 @app.route('/')
@@ -26,6 +38,7 @@ def legal():
     return render_template('legal.html')
 
 
+
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = ContactForm()
@@ -35,11 +48,16 @@ def contact():
             flash('All fields are required.')
             return render_template('contact.html', form=form)
         else:
-            return 'Form posted.'
+            msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
+            msg.body = """
+            From: %s <%s>
+            %s
+         """ % (form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
+
+        return 'Form posted.'
 
     elif request.method == 'GET':
         return render_template('contact.html', form=form)
 
 
-if __name__ == "__main__":
-    app.run()
