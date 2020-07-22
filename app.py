0ff7bd5de062e77294f03464b8d2ca23531caffd
eddiebrett <eddiebrett@hotmail.co.uk>
2020-07-22 19:18:19 +0000

    adding secret password

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, flash
 from forms import ContactForm
-from flask import Message, Mail
+from flask_mail import Message, Mail
+import os
+
+
 
 
 mail = Mail()
@@ -12,8 +15,8 @@ app.secret_key = 'development key'
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = 'bobdylan20202020@gmail.com'
-app.config["MAIL_PASSWORD"] = 'your-password'
+app.config["MAIL_USERNAME"] = 'USEREMAIL'
+app.config["MAIL_PASSWORD"] = 'USERPASSWORD'
  
 mail.init_app(app)
 
@@ -41,23 +44,22 @@ def legal():
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    form = ContactForm()
-
-    if request.method == 'POST':
-        if form.validate() is False:
-            flash('All fields are required.')
-            return render_template('contact.html', form=form)
-        else:
-            msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
-            msg.body = """
-            From: %s <%s>
-            %s
-         """ % (form.name.data, form.email.data, form.message.data)
-            mail.send(msg)
-
-        return 'Form posted.'
-
-    elif request.method == 'GET':
-        return render_template('contact.html', form=form)
-
+  form = ContactForm()
+ 
+  if request.method == 'POST':
+    if form.validate() is False:
+      flash('All fields are required.')
+      return render_template('contact.html', form=form)
+    else:
+      msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
+      msg.body = """
+      From: %s &lt;%s&gt;
+      %s
+      """ % (form.name.data, form.email.data, form.message.data)
+      mail.send(msg)
+ 
+      return render_template('contact.html', success=True)
+ 
+  elif request.method == 'GET':
+    return render_template('contact.html', form=form)
 
