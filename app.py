bd602925d01091a58d4f2c363f0c9725f306b315
eddiebrett <eddiebrett@hotmail.co.uk>
2020-07-27 14:29:21 +0000

    took out some text

--- app.py
+++ app.py
@@ -85,26 +85,3 @@ def contact():
 
 if __name__ == "__main__":
     app.run()
-
-
-
-# @app.route('/contact', methods=['GET', 'POST'])
-# def contact():
-#   form = ContactForm()
- 
-#   if request.method == 'POST':
-#     if form.validate() == False:
-#       flash('All fields are required.')
-#       return render_template('contact.html', form=form)
-#     else:
-#       msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
-#       msg.body = """
-#       From: %s &lt;%s&gt;
-#       %s
-#       """ % (form.name.data, form.email.data, form.message.data)
-#       mail.send(msg)
- 
-#       return render_template('contact.html', success=True)
- 
-#   elif request.method == 'GET':
-#     return render_template('contact.html', form=form)
