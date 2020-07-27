c71c73bdba7f8bf8b1f9197e68ae918a2dd73e97
eddiebrett <eddiebrett@hotmail.co.uk>
2020-07-27 11:12:50 +0000

    tls

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app.secret_key = 'development key'
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_SSL"] = True
-# app.config["MAIL_USE_TLS"] = True
+# app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = 'bobbydylan20202020@gmail.com'
 app.config["MAIL_PASSWORD"] = 'BobDylan2020'
 
