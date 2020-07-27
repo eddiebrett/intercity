065d2826278d317584af02011767bf4f71660f2d
eddiebrett <eddiebrett@hotmail.co.uk>
2020-07-27 11:01:13 +0000

    email update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
 # app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = 'bobdylan20202020@gmail.com'
+app.config["MAIL_USERNAME"] = 'bobbydylan20202020@gmail.com'
 app.config["MAIL_PASSWORD"] = 'BobDylan2020'
 
 mail.init_app(app)
