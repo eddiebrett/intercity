65a7426aa8594362a9c11ab6641e0101e5fefb15
eddiebrett <eddiebrett@hotmail.co.uk>
2020-07-31 11:27:03 +0000

    hello

--- static/js/home-text.js
+++ static/js/home-text.js
@@ -1,6 +1,6 @@
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml11 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letters'>$&</span>");
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: false})
   .add({
